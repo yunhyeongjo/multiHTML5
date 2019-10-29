b3b6f6437c5b6fcd8e9e7b226199e0726e0f0ad3
yunhyeongjo <zzdhtpdy@naver.com>
2019-10-29 16:48:55 +0900

    메인 메뉴 일부 디자인 완성 수정필요

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,2 +1,7 @@
-var lists = $('.member li:nth-child(n+2)');
-lists.prepend('<span aria-hidden="true">:</span>');
+var member_lists = $('.member li:nth-child(n+2)');
+var btn_menu = $('.btn-menu');
+var submenu_list = $('.sub-menu li');
+
+member_lists.prepend('<span aria-hidden="true">:</span>');
+submenu_list.addClass('icon-dot-circled');
+btn_menu.attr('role','button');
