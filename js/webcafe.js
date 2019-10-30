fbe2632374172f487a301a4638894c2e33ecdb2f
yunhyeongjo <zzdhtpdy@naver.com>
2019-10-30 09:51:04 +0900

    헤더 디자인 완성 및 비주얼 마크업 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,7 +1,21 @@
 var member_lists = $('.member li:nth-child(n+2)');
 var btn_menu = $('.btn-menu');
 var submenu_list = $('.sub-menu li');
+var menu_items = $('.menu-item');
 
 member_lists.prepend('<span aria-hidden="true">:</span>');
 submenu_list.addClass('icon-dot-circled');
 btn_menu.attr('role','button');
+
+btn_menu.on('click',function(e){
+  var current_menu_item = $(this).parent().hasClass('menu-act');
+
+  e.preventDefault();
+  
+  if(current_menu_item){
+    $(this).parent().removeClass('menu-act');
+  }else{
+     menu_items.removeClass('menu-act');
+     $(this).parent().addClass('menu-act');
+  }
+})
